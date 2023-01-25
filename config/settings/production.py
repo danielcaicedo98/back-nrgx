b91b19e4eabba685b2379b846b04246cc04a7bfa
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2023-01-24 20:17:49 -0500

    initial

---
+++ config/settings/production.py
@@ -0,0 +1,129 @@
+"""Production settings."""
+
+from .base import *  # NOQA
+from .base import env
+
+# Base
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[
+    "localhost",
+    "3.20.82.253",
+    "nrgx.tortascrispan.com"
+])
+PRODUCTION = True
+
+# Databases
+DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # NOQA
+
+# # Cache
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': env('REDIS_URL'),
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#             'IGNORE_EXCEPTIONS': True,
+#         }
+#     }
+# }
+
+# Security
+SESSION_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = "DENY"
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_HSTS_SECONDS = 60
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
+# SECURE_HSTS_PRELOAD = env.bool('DJANGO_SECURE_HSTS_PRELOAD', default=True)
+# SECURE_CONTENT_TYPE_NOSNIFF = env.bool('DJANGO_SECURE_CONTENT_TYPE_NOSNIFF', default=True)
+
+# Storages
+
+# Static  files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = 'app/static/'
+
+# Media
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+
+# Templates
+TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
+    (
+        'django.template.loaders.cached.Loader',
+        [
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        ]
+    ),
+]
+
+# Email
+
+EMAIL_FROM = env('DJANGO_EMAIL_FROM')
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND')
+EMAIL_USE_TLS = env('DJANGO_EMAIL_USE_TLS')
+EMAIL_HOST = env('DJANGO_EMAIL_HOST')
+EMAIL_HOST_USER = env('DJANGO_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('DJANGO_EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('DJANGO_EMAIL_PORT')
+
+# Admin
+ADMIN_URL = env('DJANGO_ADMIN_URL')
+
+# Gunicorn
+INSTALLED_APPS += ['gunicorn']  # noqa F405
+
+# WhiteNoise
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')  # noqa F405
+
+
+# Logging
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['console', 'mail_admins'],
+            'propagate': True
+        }
+    }
+}
---
+++ django_api/contratos/models/contratos.py
@@ -0,0 +1,52 @@
+"""Contratos Model"""
+
+# Django
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Utilities
+from django_api.utils.models import DateBaseModel
+
+
+class Contratos(DateBaseModel):
+
+    class TipoDeUsoChoices(models.TextChoices):
+        RECIDENCIAL = 1
+        COMERCIAL = 2
+
+    class EstadoChoices(models.TextChoices):
+        ACTIVO = 1
+        INACTIVO = 2
+        SUSPENDIDO = 3
+
+    class EstadoDePago(models.TextChoices):
+        ALDIA = 1, _('AL DÍA')
+        ENMORA = 2, _('EN MORA')
+
+    cliente = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    direccion_instalacion = models.CharField('Dirección de instalación', max_length=150)
+    latitud = models.DecimalField('Latitud', max_digits=10, decimal_places=8, default=0)
+    longitud = models.DecimalField('Longitud', max_digits=10, decimal_places=8, default=0)
+    ciudad = models.ForeignKey('utils.Ciudades', on_delete=models.CASCADE)
+    tipo_de_uso = models.CharField(
+        'Tipo de uso',
+        choices=TipoDeUsoChoices.choices,
+        max_length=1,
+        default=TipoDeUsoChoices.RECIDENCIAL
+    )
+    estrato = models.IntegerField('Estrato socioeconomico')
+    estado = models.CharField('Estado', choices=EstadoChoices.choices, default=EstadoChoices.INACTIVO, max_length=1)
+    estado_de_pago = models.CharField('Estado de pago', choices=EstadoDePago.choices, max_length=1, null=True)
+    servicios = models.ManyToManyField('servicios.Servicios', related_name='servicios_contrato')
+    fecha_instalacion = models.DateTimeField('Fecha de instalación', help_text="Fecha y hora de instalación")
+
+
+    def __str__(self):
+        return "Contrato {}".format(self.id)
+
+    class Meta(DateBaseModel.Meta):
+        db_table = 'contratos'
+        managed = True
+        verbose_name = 'contrato'
+        verbose_name_plural = 'contratos'
+
